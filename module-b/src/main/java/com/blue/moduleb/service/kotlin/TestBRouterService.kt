37ea60aa65ddae7cca2177a79e02c4e7b55a0e5e
blue <xuyf@codoon.com>
2018-12-13 11:35:38 +0800

    update kotlin service

--- module-b/src/main/java/com/blue/moduleb/service/kotlin/TestBRouterService.kt
+++ module-b/src/main/java/com/blue/moduleb/service/kotlin/TestBRouterService.kt
@@ -12,31 +12,26 @@ import com.blue.xrouter.annotation.Router
  * kotlin provides service
  * Created by blue on 2018/9/28.
  */
-object TestBRouterService {
 
-    @JvmStatic
-    @Router("toast_kotlin")
-    fun toast(context: Context, routerParams: XRouterParams): XRouterResult {
-        Toast.makeText(context, "toast from other module", Toast.LENGTH_SHORT).show()
-        return XRouterResult.Builder().build()
-    }
+@Router("toast_kotlin")
+fun toast(context: Context, routerParams: XRouterParams): XRouterResult {
+    Toast.makeText(context, "toast from other module", Toast.LENGTH_SHORT).show()
+    return XRouterResult.Builder().build()
+}
 
-    @JvmStatic
-    @Router("getSum_kotlin", async = true)
-    fun getSum(context: Context, routerParams: XRouterParams, callback: XRouterCallback?) {
-        val fragment = routerParams.obj as Fragment
-        // TODO
-        val a = routerParams.data.getInt("a")
-        val b = routerParams.data.getInt("b")
-        val result = a + b
+@Router("getSum_kotlin", async = true)
+fun getSum(context: Context, routerParams: XRouterParams, callback: XRouterCallback?) {
+    val fragment = routerParams.obj as Fragment
+    // TODO
+    val a = routerParams.data.getInt("a")
+    val b = routerParams.data.getInt("b")
+    val result = a + b
 
-        callback?.onRouterSuccess(XRouterResult.Builder().data("result", result).build())
-    }
+    callback?.onRouterSuccess(XRouterResult.Builder().data("result", result).build())
+}
 
-    @JvmStatic
-    @Router("getFragment_kotlin")
-    fun getFragment(context: Context, routerParams: XRouterParams): XRouterResult {
-        val fragment = Fragment()
-        return XRouterResult.Builder().obj(fragment).build()
-    }
+@Router("getFragment_kotlin")
+fun getFragment(context: Context, routerParams: XRouterParams): XRouterResult {
+    val fragment = Fragment()
+    return XRouterResult.Builder().obj(fragment).build()
 }
