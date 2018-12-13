e07b0716454879c0b3ee6cac3be5545b030004df
blue <xuyf@codoon.com>
2018-12-13 16:32:05 +0800

    print router result

--- xrouter-core/src/main/java/com/blue/xrouter/XRouterCallBack.kt
+++ xrouter-core/src/main/java/com/blue/xrouter/XRouterCallBack.kt
@@ -1,14 +1,18 @@
 package com.blue.xrouter
 
+import com.blue.xrouter.tools.Logger
+
 /**
  * Created by blue on 2018/9/29.
  */
-abstract class XRouterCallback {
+open class XRouterCallback {
 
     /**
      * data bundle limit 1Mb
      */
-    abstract fun onRouterSuccess(routerResult: XRouterResult)
+    open fun onRouterSuccess(routerResult: XRouterResult) {
+        Logger.d("XRouter", "routerResult:$routerResult")
+    }
 
     open fun onRouterError(routerResult: XRouterResult) {
     }
